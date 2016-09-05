c740eeb858de0185bf81096227b879462e1adfb4
Leo_Persson <kangosjarvi97@gmail.com>
2016-09-05 13:31:20 +0200

    Testpatch

--- assignment1/src/assignment1/SchoolbookCalc.java
+++ assignment1/src/assignment1/SchoolbookCalc.java
@@ -1,10 +1,40 @@
 package assignment1;
 
-public class SchoolbookCalc {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class SchoolbookCalc
+{
+	public static void main(String[] args)
+	{
+		
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
