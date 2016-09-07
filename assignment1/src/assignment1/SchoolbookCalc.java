6c1b9d69c48be7e15a413ee651ae6b170215156a
Leo_Persson <kangosjarvi97@gmail.com>
2016-09-07 17:24:46 +0200

    PartialCommentpatch

--- assignment1/src/assignment1/SchoolbookCalc.java
+++ assignment1/src/assignment1/SchoolbookCalc.java
@@ -7,56 +7,56 @@ public class SchoolbookCalc
 	{
 		//Ameera's part of code |
 		//						V
-		Scanner read = new Scanner(System.in);			//I took the liberty of creating a Scanner and input up here :) Leo 2016/09/07 11:48
+		
+		/**
+		 * Leo Persson began creating input and errorhandling 2016-09-07 11:48
+		 */
+		Scanner read = new Scanner(System.in);																						//Declaration of input variables and creation of Scanner
 		char operand;
 		int firstnum, secondnum;
 		
-		System.out.print("Welcome to DIT948 Calculator!\nPlease input operand (+, -, *, /): ");
-		operand = read.next().charAt(0);
-		while(operand != '+' && operand != '-' && operand != '*' && operand != '/')
+		System.out.print("Welcome to DIT948 Calculator!\nPlease input operand (+, -, *, /): ");										//Welcome message
+		operand = read.next().charAt(0);																							//Input string, first character is char used as operand
+		while(operand != '+' && operand != '-' && operand != '*' && operand != '/')													//Checking if char operand is actually an operand, if not, returns an error and prompts user to input new value
 		{
-			System.out.print("ERROR: Input \'" + operand + "\' was not accepted operand. Please input operand (+, -, *, /): ");
-			operand = read.next().charAt(0);
+			System.out.print("ERROR: Input \'" + operand + "\' was not accepted operand. Please input operand (+, -, *, /): ");		//Output error message and prompt userinput
+			operand = read.next().charAt(0);																						//Input new userinput
 		}
-		switch(operand)
+		switch(operand)																												//Switch that checks for operand and makes sure program is using right arithmetic term
 		{
 		case '+':
-			System.out.print("Operand selected. Please input first term of calculation (0-99): ");
+			System.out.print("Operand selected. Please input first term of calculation (0-99): ");									//Addition
 			break;
 		case '-':
-			System.out.print("Operand selected. Please input first term of calculation (0-99): ");
+			System.out.print("Operand selected. Please input first term of calculation (0-99): ");									//Subtraction
 			break;
 		case '*':
-			System.out.print("Operand selected. Please input first factor of calculation (0-99): ");
+			System.out.print("Operand selected. Please input first factor of calculation (0-99): ");								//Multiplication
 			break;
 		case '/':
-			System.out.print("Operand selected. Please input dividend (0-99): ");
+			System.out.print("Operand selected. Please input dividend (0-99): ");													//Division
 			break;
 		}
-		while(!read.hasNextInt())
+		while(!read.hasNextInt())																									//
 		{
 			String temp = read.nextLine();
-			switch(operand)
+			switch(operand)																											//Switch that checks for operand and makes sure error messages proper arithmetic terms
 			{
 			case '+':
-				System.out.print("ERROR: Input \'" + temp +  "\' was not accepted integer. Please input first term of calculation (0-99): ");
+				System.out.print("ERROR: Input \'" + temp +  "\' was not accepted integer. Please input first term of calculation (0-99): ");	//Addition
 				break;
 			case '-':
-				System.out.print("ERROR: Input \'" + temp +  "\' was not accepted integer. Please input first term of calculation (0-99): ");
+				System.out.print("ERROR: Input \'" + temp +  "\' was not accepted integer. Please input first term of calculation (0-99): ");	//Subtraction
 				break;
 			case '*':
-				System.out.print("ERROR: Input \'" + temp +  "\' was not accepted integer. Please input first factor of calculation (0-99): ");
+				System.out.print("ERROR: Input \'" + temp +  "\' was not accepted integer. Please input first factor of calculation (0-99): ");	//Multiplication
 				break;
 			case '/':
-				System.out.print("ERROR: Input \'" + temp +  "\' was not accepted integer. Please input dividend (0-99): ");
+				System.out.print("ERROR: Input \'" + temp +  "\' was not accepted integer. Please input dividend (0-99): ");					//Division
 				break;
 			}
 		}
 		firstnum = read.nextInt();
-		
-		
-		
-		
 		
 		switch(operand)
 		{
@@ -96,11 +96,13 @@ public class SchoolbookCalc
 		
 		System.out.println("Normal calculation: " + firstnum + " " + operand + " " + secondnum);
 		
+		/**
+		 * Leo Persson finished input and errorhandling 2016-09-07 17:00
+		 */
 		
 		
 		
 		
-		//I am a happy face :)
 		
 		
 		
