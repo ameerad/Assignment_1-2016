50b819becdf12ee23ddd087a74b792a203bd3cd6
Leo_Persson <kangosjarvi97@gmail.com>
2016-09-07 12:05:39 +0200

    Inputpatch

--- assignment1/src/assignment1/SchoolbookCalc.java
+++ assignment1/src/assignment1/SchoolbookCalc.java
@@ -1,4 +1,5 @@
 package assignment1;
+import java.util.Scanner;
 
 public class SchoolbookCalc
 {
@@ -6,8 +7,18 @@ public class SchoolbookCalc
 	{
 		//Ameera's part of code |
 		//						V
-		
-		
+		Scanner read = new Scanner(System.in);			//I took the liberty of creating a Scanner and input up here :) Leo 2016/09/07 11:48
+		char operand;
+		int firstnum, secondnum;
+		System.out.print("Welcome to DIT948 Calculator!\nPlease input operand (+, -, *, /): ");
+		operand = read.next().charAt(0);
+		while(operand != '+' || operand != '-' || operand != '*' || operand != '/')
+		{
+			System.out.print("ERROR: Input was not accepted operand. Please input operand (+, -, *, /): ");
+			operand = read.next().charAt(0);
+			System.out.println(operand);
+		}
+		System.out.println(operand);					//Not working for some reason... Leo 2016/09/07 12:02
 		
 		
 		
